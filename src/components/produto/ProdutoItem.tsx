39cff6fe807c36a54520cf41a54a6d4e2d2d5df2
Gabriel Correia <gabriel.correia@suporterenova.onmicrosoft.com>
2024-10-04 17:37:03 -0300

    feat: finalização do front-end

--- src/components/produto/ProdutoItem.tsx
+++ src/components/produto/ProdutoItem.tsx
@@ -1,4 +1,6 @@
+'use client'
 import { Produto } from "@/core";
+import { IconShoppingCartPlus } from "@tabler/icons-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,12 +13,40 @@ export default function ProdutoItem(props: ProdutoItemProps) {
   return (
     <Link
       href={`/produto/${produto.id}`}
-      className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]"
+      className="flex flex-col bg-violet-dark border border-white/20 rounded-xl relative max-w-[350px]"
     >
       <div className="h-48 w-full relative">
-        <Image src={produto.imagem} fill alt="Imagem Produto"></Image>
+        <Image
+          src={produto.imagem}
+          alt="Imagem Produto"
+          className="object-contain"
+          fill
+        ></Image>
       </div>
-      {props.produto.nome}
+      <div className="flex-1 flex flex-col p-5 gap-3 border-t border-white/20">
+        <span className="text-lg font-semibold">{props.produto.nome}</span>
+        <span className="text-sm border-b border-dashed self-start">
+          {produto.especificacoes.destaque}
+        </span>
+        <div className="flex-1"></div>
+        <div className="flex flex-col">
+          <span className="text-sm text-gray-400 line-through">
+            de {props.produto.precoBase}
+          </span>
+          <span className="text-xl font-semibold text-emerald-400">
+            por {props.produto.precoPromocional}
+          </span>
+        </div>
+        <button className="flex bg-violet-700 rounded-full justify-center h-8 gap-2 items-center hover:border-2 border-emerald-500"
+          onClick={(e)=>{
+            e.preventDefault()
+            console.log("Adicionado ao carrinho")
+          }}
+        >
+          <IconShoppingCartPlus size={20}></IconShoppingCartPlus>
+          <span>adicionar</span>
+        </button>
+      </div>
     </Link>
   );
 }
